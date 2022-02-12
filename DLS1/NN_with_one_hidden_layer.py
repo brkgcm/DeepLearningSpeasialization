107913fd177b34fee1d5a57cccdd6f68c644ac9a
Berke Gocmen <berkegocmen@hotmail.com>
2022-02-12 23:57:36 +0300

    imports

--- DLS1/NN_with_one_hidden_layer.py
+++ DLS1/NN_with_one_hidden_layer.py
@@ -1,1 +1,9 @@
-import numpy as np
+# Import  the required libraries    
+import numpy as np  
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+from PIL import Image
+from scipy import ndimage
+
+
