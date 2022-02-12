6c530f74b75cf10a3bd5f64935fdf686d1cc9787
Berke Gocmen <berkegocmen@hotmail.com>
2022-02-13 00:05:23 +0300

    propagation fuction

--- DLS1/NN_with_one_hidden_layer.py
+++ DLS1/NN_with_one_hidden_layer.py
@@ -15,3 +15,21 @@ def initilize_with_zeros(dim):
     b = 0
     return w,b
 
+# Forward and backward propagation
+def propagate(w, b, X, Y):
+    #Forward Propagation
+    m = X.shape[1]
+    A = sigmoid(np.dot(w.T,X)+b)
+    cost = np.sum((Y*np.log(A))+((1-Y)*np.log(1-A)))/(-m)
+
+    #Backward Propagation
+    dw = np.dot(X, (A-Y).T)/m
+    db = np.sum(A-Y)/m
+
+    cost = np.squeeze(np.array(cost))
+
+    grads = {'dw':dw, 'db':db}
+
+    return grads, cost
+
+
