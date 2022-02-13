56304b58de04ebea561a808c1c8dd3147354adc7
Berke Gocmen <berkegocmen@hotmail.com>
2022-02-13 15:43:01 +0300

    backward propagation

--- DLS1/one_hiden_layer_nn.py
+++ DLS1/one_hiden_layer_nn.py
@@ -46,4 +46,24 @@ def compute_cost(A2, Y):
     cost = np.squeeze(np.array(cost))
     return cost
 
-    
+
+def backward_propagation(parameters, cache, X, Y):
+    m = X.shape[1]
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+
+    A1 = cache['a1']
+    A2 = cache['a2']
+
+    dZ2 = A2 - Y
+    dW2 = np.dot(dZ2, A1.T)/m
+    db2 = np.sum(dZ2, axis=1, keepdims=True)/m
+
+    dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+    dW1 = np.dot(dZ1, X.T)/m
+    db1 = np.sum(dZ1, axis=1 , keepdims=True)/m
+
+    grads ={'dw1':dW1, 'db1':db1, 'dw2':dW2, 'db2':db2}
+    return grads
+
+
