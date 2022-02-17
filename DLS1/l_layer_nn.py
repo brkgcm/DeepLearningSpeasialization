c1c9ed86038cea1d985caf681291276c91f8fdb4
Berke Gocmen <berkegocmen@hotmail.com>
2022-02-17 17:08:41 +0300

    backward propagation

--- DLS1/l_layer_nn.py
+++ DLS1/l_layer_nn.py
@@ -1,4 +1,5 @@
 #Packages
+from turtle import circle
 import numpy as np
 
 #sigmoid functuion
@@ -69,6 +70,71 @@ def compute_cost(AL, Y):
     return cost
 
 
+#sigmoid backward propagation
+def sigmoid_backward(dA, activation_cache):
+    Z = activation_cache
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    return dZ
+
+
+# relu backward propagation
+def relu_backward(dA, activation_cache):
+    Z = activation_cache
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    return dZ
+
+#linear part of the backward propagation
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = np.dot(dZ, A_prev.T)/m
+    db = np.sum(dZ, axis=1, keepdims=True)/m
+    dA_prev = np.dot(W.T, dZ)
+
+    return dA_prev, dW, db
+
+
+# backward propagation activation->linear
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+    if activation = 'relu':
+        dZ = relu_backward(dA, cache)
+        dA_prev, dW, db =  linear_backward(dZ, cache)
+    if activation = 'sigmoid':
+        dZ = sigmoid_backward(dA, cache)
+        dA_prev, dW, db = linear_backward(dZ, cache)
+    return dA_prev, dW, db
+
+
+# L layer backward propagation
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches) # the number of layers
+    m = AL.shape[1] # number of examples
+    Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
+
+    #Gradient of the cost
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    # Lth layer of (SIGMOID -> LINEAR) gradients.
+    current_cache = caches[-1]
+    dA_prev_temp, dW_temp, db_temp = linear_activation_backward(dAL, current_cache, activation='sigmoid')
+    grads['dA' + str(L-1)] = dA_prev_temp
+    grads['dW' + str(L)] = dW_temp
+    grads['db' + str(L)] = db_temp
+
+    #Loop from l=L-2 to l=0
+    for l in reversed(range(L-1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(dA_prev_temp, current_cache, activation='relu')
+        grads['dA' + str(l)] = dA_prev_temp
+        grads['dW' + str(l+1)] = dW_temp
+        grads['db' + str(l+1)] = db_temp
+    
+    return grads
 
 
     
