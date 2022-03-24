eb8dd2d7a445c53c36e2042773ef402ddaabddc0
Berke Gocmen <berkegocmen@hotmail.com>
2022-03-25 01:05:02 +0300

    dimension fix

--- DLS4/cnn.py
+++ DLS4/cnn.py
@@ -3,5 +3,5 @@ import numpy as np
 
 #Pad images with zeros
 def zero_pad(X, pad):
-    X_pad = np.pad(X, ((0, 0), (1, 1), (3, 3), (0, 0)), 'constant', constant_values=(0, 0))
+    X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values=(0, 0))
     return X_pad
