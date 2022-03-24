4663ff0d64d2fd58b26cc8bcde3019e136e06e47
Berke Gocmen <berkegocmen@hotmail.com>
2022-03-25 01:28:03 +0300

    Single step of convolution operation

--- DLS4/cnn.py
+++ DLS4/cnn.py
@@ -5,3 +5,10 @@ import numpy as np
 def zero_pad(X, pad):
     X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values=(0, 0))
     return X_pad
+
+# Single step of convolution
+def cov_single_step(a_slice_prev, W, b)
+    s = np.multiply(a_slice_prev, W)
+    Z = np.sum(s, dtype=np.float64)
+    Z = Z + np.asscalar(b)
+    return Z
